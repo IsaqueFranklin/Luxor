5e9c9101fad6376ebf4b35fa467efc4353cbb7ad
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-07-11 22:11:27 -0300

    Added modules completed, contents completed, scores on each quizz, total score to user Schema.

--- api/models/User.js
+++ api/models/User.js
@@ -1,16 +1,29 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
+const quizDataSchema = new Schema({
+    quizId: {
+      type: {type: mongoose.Schema.Types.ObjectId, ref:'Conteudo'},
+      required: true
+    },
+    quizScore: {
+      type: String,
+      required: true
+    },
+});
+
 const UserSchema = new Schema({
     name: String,
     username: {type:String, unique:true},
     email: {type:String, unique:true},
     admin: {type:Boolean, default:false},
     profileImg: [String],
-    bio: String,
-    instagram: String,
     password: String,
     fullUser: {type: Boolean, default: true},
+    completedContents: [{type: mongoose.Schema.Types.ObjectId, ref:'Conteudo'}],
+    completedModules: [{type: mongoose.Schema.Types.ObjectId, ref:'Book'}],
+    quizzScores: [quizDataSchema],
+    totalScore: String,
 })
 
 const UserModel = mongoose.model('User', UserSchema);
